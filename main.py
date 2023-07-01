c581455e081d8c9571a60a0bae29d4bcf33cc7dc
btlee215 <31486181+btlee215@users.noreply.github.com>
2023-07-01 14:40:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
 import streamlit as st
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-xxl"
+headers = {"Authorization": "Bearer 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"}
 
 def main():
     st.title("Patient Email Assistant")
     st.subheader("Input Email")
-
-    # Load the T5 model and tokenizer
-    model_name = "google/flan-t5-xxl"
-    model = T5ForConditionalGeneration.from_pretrained(model_name)
-    tokenizer = T5Tokenizer.from_pretrained(model_name)
 
     # Input text area
     email_text = st.text_area("Enter the email", height=200)
 
     # Generate button
     if st.button("Generate Response"):
-        response = generate_response(email_text, model, tokenizer)
+        response = generate_response(email_text)
         st.subheader("Generated Response")
         st.write(response)
 
-def generate_response(email_text, model, tokenizer):
-    # Tokenize the input text
-    input_ids = tokenizer.encode(email_text, return_tensors="pt")
-
-    # Generate the response
-    output = model.generate(input_ids)
-
-    # Decode the generated output
-    response = tokenizer.decode(output[0], skip_special_tokens=True)
+def generate_response(email_text):
+    payload = {"inputs": email_text}
+    response = query(payload)
     return response
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
 if __name__ == "__main__":
     main()
