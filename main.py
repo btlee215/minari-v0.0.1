e56ba67caa6b6103039dad3605f80d95bf67d2c2
btlee215 <31486181+btlee215@users.noreply.github.com>
2023-07-01 15:10:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 import streamlit as st
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import requests
 
+API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-xxl"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
 def main():
     st.title("Patient Email Assistant")
     st.subheader("Input Email")
 
+    # Input text area
     email_text = st.text_area("Enter the email", height=200)
-    
+
+    # Generate button
     if st.button("Generate Response"):
         response = generate_response(email_text)
         st.subheader("Generated Response")
-        st.write(response)
+        edited_response = st.text_area("Edit the response", value=response["generated_text"], height=200)
+        st.write("Modified Response:")
+        st.write(edited_response)
 
-        
 def generate_response(email_text):
-    model_name = "google/flan-t5-xxl"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    prompt = "As an experienced clinician, I understand your concerns. Based on your email, I would like to provide you with the following insights and possible next steps:"
+    input_text = f"{prompt}\n{email_text}"
 
-    inputs = tokenizer.encode(email_text, return_tensors="pt")
-    outputs = model.generate(inputs, max_length=1000)
-    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-
+    payload = {
+        "inputs": input_text,
+    }
+    response = query(payload)
     return response
-
 
 if __name__ == "__main__":
     main()
