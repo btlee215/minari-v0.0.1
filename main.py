cbad05c05075e875ce2cb8449e90cc903c9d16b9
btlee215 <31486181+btlee215@users.noreply.github.com>
2023-06-30 22:11:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,43 @@
 import streamlit as st
 import openai
 
-# Set up OpenAI API credentials
-openai.api_key = "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"
-
 def generate_response(email_text):
-    # Construct the prompt with a system message
-    prompt = f"Prompt: You are a doctor with great bedside manner. You received an email from a patient:\n\n{email_text}\n\nResponse:"
+    # Set up OpenAI API credentials
+    openai.api_key = "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"
 
-    # Make a request to the OpenAI API for text generation
+    # Generate response using OpenAI API
     response = openai.Completion.create(
-        engine="davinci-codex",  # GPT-3.5 engine
-        prompt=prompt,
+        engine="text-davinci-003",
+        prompt=email_text,
         max_tokens=100,
         temperature=0.7,
         n=1,
-        stop=None
+        stop=None,
+        top_p=None,
+        frequency_penalty=None,
+        presence_penalty=None,
+        log_level=None
     )
-    
-    # Extract the generated response from the API response
-    generated_text = response.choices[0].text.strip()
-    
-    # Return the generated response
-    return generated_text
+
+    return response.choices[0].text.strip()
 
 def main():
-    # Set the title and description of the app
-    st.title("Email Response Generator")
-    st.markdown("Enter an email and generate a response with the voice of a clinician with great bedside manner.")
+    # Set page title
+    st.set_page_config(page_title="Email Assistant")
 
-    # Input email text
-    email_text = st.text_area("Enter the email text", height=200)
+    # Set up sidebar
+    st.sidebar.title("Email Assistant")
 
-    # Generate response button
+    # Get user input
+    email_text = st.text_area("Enter the email text")
+
+    # Generate response
     if st.button("Generate Response"):
         if email_text:
-            # Generate the response
             response = generate_response(email_text)
-            
-            # Display the generated response
-            st.markdown("## Generated Response")
-            st.write(response)
+            st.success(response)
         else:
-            st.warning("Please enter an email text.")
+            st.warning("Please enter some email text.")
 
 if __name__ == "__main__":
     main()
