0b4d900007cdcf40a3935a01dca6a294dd1bae67
btlee215 <31486181+btlee215@users.noreply.github.com>
2023-07-02 14:55:02 -0400

    Update main.py to use medalpaca

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-xxl"
+API_URL = "https://api-inference.huggingface.co/models/medalpaca/medalpaca-13b"
 headers = {"Authorization": "Bearer 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"}
 
 def main():
